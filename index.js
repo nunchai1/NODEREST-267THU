f805465506c3f4c6b9755041803daa2a7610adf4
Your Name <your-email@example.com>
2025-02-13 08:34:40 +0700

    Add CRUD functionality for books and improve server scripts

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req,res) => {
-    res.send("Hello Word!");
+app.get("/", (req, res) => {
+  res.send("Hello Word!");
 });
 
 app.listen(port, () => {
-        console.log(`Server is listening on http://localhost:${port}`);
-    });
+  console.log(`Server is listening on http://localhost:${port}`);
+});
